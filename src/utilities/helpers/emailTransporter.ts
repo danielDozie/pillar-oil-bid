886cd916a7c1ba81edddd71322e1c7579e47bec4
Daniel Dozie <31423288+danielDozie@users.noreply.github.com>
2024-05-07 01:58:41 +0100

    Update emailTransporter.ts

--- src/utilities/helpers/emailTransporter.ts
+++ src/utilities/helpers/emailTransporter.ts
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 // Create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.MAIL_USERNAME,
         pass: process.env.MAIL_PASSWORD,
